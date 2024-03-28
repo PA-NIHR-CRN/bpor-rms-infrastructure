b26e3ed927f7794f8a552b566b5d89f16741bd1b
yousef nasser <yousef.nasser@paconsulting.com>
2024-03-28 17:01:22 +0000

    remove ecs specific log

--- modules/cloudwatch_alarms/ecs-error-alarms.tf
+++ modules/cloudwatch_alarms/ecs-error-alarms.tf
@@ -1,37 +1,37 @@
-resource "aws_cloudwatch_log_metric_filter" "ecs_error_filter" {
-  name           = "${var.account}-cloudwatch-${var.env}-${var.app}-web-error-filter"
-  pattern        = "{$.level = 50 && $.err.config.params.grant_type != \"refresh_token\"}"
-  log_group_name = var.web_log_group
+# resource "aws_cloudwatch_log_metric_filter" "ecs_error_filter" {
+#   name           = "${var.account}-cloudwatch-${var.env}-${var.app}-web-error-filter"
+#   pattern        = "{$.level = 50 && $.err.config.params.grant_type != \"refresh_token\"}"
+#   log_group_name = var.web_log_group
 
-  metric_transformation {
-    name      = "ErrorCount"
-    namespace = "SE/${var.env}-se-ecs-web-errors"
-    value     = "1"
-  }
-}
+#   metric_transformation {
+#     name      = "ErrorCount"
+#     namespace = "SE/${var.env}-se-ecs-web-errors"
+#     value     = "1"
+#   }
+# }
 
-resource "aws_cloudwatch_metric_alarm" "ecs_error_alarm" {
-  alarm_name          = "${var.account}-cloudwatch-${var.env}-${var.app}-web-error-alarm"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "ErrorCount"
-  namespace           = "SE/${var.env}-se-ecs-web-errors"
-  period              = "60"
-  statistic           = "Minimum"
-  threshold           = "1"
+# resource "aws_cloudwatch_metric_alarm" "ecs_error_alarm" {
+#   alarm_name          = "${var.account}-cloudwatch-${var.env}-${var.app}-web-error-alarm"
+#   comparison_operator = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods  = "1"
+#   metric_name         = "ErrorCount"
+#   namespace           = "SE/${var.env}-se-ecs-web-errors"
+#   period              = "60"
+#   statistic           = "Minimum"
+#   threshold           = "1"
 
-  alarm_description = "Alarm when an error has occured for the Sponsor Engagement Web"
-  alarm_actions     = var.env == "prod" ? [var.sns_topic, var.sns_topic_service_desk] : [var.sns_topic]
-  ok_actions        = var.env == "prod" ? [var.sns_topic, var.sns_topic_service_desk] : [var.sns_topic]
+#   alarm_description = "Alarm when an error has occured for the Sponsor Engagement Web"
+#   alarm_actions     = var.env == "prod" ? [var.sns_topic, var.sns_topic_service_desk] : [var.sns_topic]
+#   ok_actions        = var.env == "prod" ? [var.sns_topic, var.sns_topic_service_desk] : [var.sns_topic]
 
-  treat_missing_data = "notBreaching"
+#   treat_missing_data = "notBreaching"
 
-  tags = {
-    Name        = "${var.account}-cloudwatch-${var.env}-${var.app}-web-error-alarm"
-    Environment = var.env
-    System      = var.app
-  }
-}
+#   tags = {
+#     Name        = "${var.account}-cloudwatch-${var.env}-${var.app}-web-error-alarm"
+#     Environment = var.env
+#     System      = var.app
+#   }
+# }
 
 
 
