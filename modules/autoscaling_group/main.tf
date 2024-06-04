0094cc8ee02c0812c40ffb85c90b567258e8a337
yousef nasser <yousef.nasser@paconsulting.com>
2024-06-04 14:58:04 +0100

    fmt

--- modules/autoscaling_group/main.tf
+++ modules/autoscaling_group/main.tf
@@ -12,7 +12,7 @@ resource "aws_appautoscaling_scheduled_action" "my_service" {
   service_namespace  = aws_appautoscaling_target.my_service[0].service_namespace
   resource_id        = aws_appautoscaling_target.my_service[0].resource_id
   scalable_dimension = aws_appautoscaling_target.my_service[0].scalable_dimension
-  schedule           = "cron(0 0 18 ? * MON-FRI *)"  
+  schedule           = "cron(0 0 18 ? * MON-FRI *)"
 
   scalable_target_action {
     max_capacity = 0
