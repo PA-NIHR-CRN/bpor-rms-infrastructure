c8b2ba2c37d1fe67cdfa7b6544fa5dbf0a9918c9
yousef nasser <yousef.nasser@paconsulting.com>
2024-06-05 12:58:10 +0100

    correct time

--- modules/autoscaling_group/main.tf
+++ modules/autoscaling_group/main.tf
@@ -6,10 +6,10 @@ resource "aws_appautoscaling_target" "my_service" {
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
   lifecycle {
-    ignore_changes = [ 
-        min_capacity,
-        max_capacity
-     ]
+    ignore_changes = [
+      min_capacity,
+      max_capacity
+    ]
   }
 }
 resource "aws_appautoscaling_scheduled_action" "my_service" {
@@ -18,7 +18,7 @@ resource "aws_appautoscaling_scheduled_action" "my_service" {
   service_namespace  = aws_appautoscaling_target.my_service[0].service_namespace
   resource_id        = aws_appautoscaling_target.my_service[0].resource_id
   scalable_dimension = aws_appautoscaling_target.my_service[0].scalable_dimension
-  schedule           = "cron(0 45 11 ? * MON-FRI *)"
+  schedule           = "cron(0 0 5 ? * MON-FRI *)"
 
   scalable_target_action {
     min_capacity = 0
@@ -31,7 +31,7 @@ resource "aws_appautoscaling_scheduled_action" "my_service_scale_out" {
   service_namespace  = aws_appautoscaling_target.my_service[0].service_namespace
   resource_id        = aws_appautoscaling_target.my_service[0].resource_id
   scalable_dimension = aws_appautoscaling_target.my_service[0].scalable_dimension
-  schedule           = "cron(0 55 11 ? * MON-FRI *)"
+  schedule           = "cron(0 0 17 ? * MON-FRI *)"
 
   scalable_target_action {
     min_capacity = 1
