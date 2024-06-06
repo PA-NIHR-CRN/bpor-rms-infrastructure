ea2210506f6c747731fe4f1580866c1869ac0b9e
yousef nasser <yousef.nasser@paconsulting.com>
2024-06-06 12:11:10 +0100

    swap time

--- modules/autoscaling_group/main.tf
+++ modules/autoscaling_group/main.tf
@@ -18,7 +18,7 @@ resource "aws_appautoscaling_scheduled_action" "my_service" {
   service_namespace  = aws_appautoscaling_target.my_service[0].service_namespace
   resource_id        = aws_appautoscaling_target.my_service[0].resource_id
   scalable_dimension = aws_appautoscaling_target.my_service[0].scalable_dimension
-  schedule           = "cron(0 0 5 ? * MON-FRI *)"
+  schedule           = "cron(0 0 17 ? * MON-FRI *)"
 
   scalable_target_action {
     min_capacity = 0
@@ -31,7 +31,7 @@ resource "aws_appautoscaling_scheduled_action" "my_service_scale_out" {
   service_namespace  = aws_appautoscaling_target.my_service[0].service_namespace
   resource_id        = aws_appautoscaling_target.my_service[0].resource_id
   scalable_dimension = aws_appautoscaling_target.my_service[0].scalable_dimension
-  schedule           = "cron(0 0 17 ? * MON-FRI *)"
+  schedule           = "cron(0 0 5 ? * MON-FRI *)"
 
   scalable_target_action {
     min_capacity = 1
