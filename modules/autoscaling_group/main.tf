7da130ea3c7dd526f96988c14606716104ee6087
Julian Pool <89917684+julianpool-pa@users.noreply.github.com>
2025-11-25 17:17:48 +0000

    updated ecs scaling cron schedule format

--- modules/autoscaling_group/main.tf
+++ modules/autoscaling_group/main.tf
@@ -18,7 +18,7 @@ resource "aws_appautoscaling_scheduled_action" "my_service" {
   service_namespace  = aws_appautoscaling_target.my_service[0].service_namespace
   resource_id        = aws_appautoscaling_target.my_service[0].resource_id
   scalable_dimension = aws_appautoscaling_target.my_service[0].scalable_dimension
-  schedule           = "cron(0 0 18 ? * MON-FRI *)"
+  schedule           = "cron(0 18 ? * MON-FRI *)"
   timezone           = "Europe/London"
 
   scalable_target_action {
@@ -32,7 +32,7 @@ resource "aws_appautoscaling_scheduled_action" "my_service_scale_out" {
   service_namespace  = aws_appautoscaling_target.my_service[0].service_namespace
   resource_id        = aws_appautoscaling_target.my_service[0].resource_id
   scalable_dimension = aws_appautoscaling_target.my_service[0].scalable_dimension
-  schedule           = "cron(0 0 6 ? * MON-FRI *)"
+  schedule           = "cron(0 6 ? * MON-FRI *)"
   timezone           = "Europe/London"
 
   scalable_target_action {
